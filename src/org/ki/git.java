16d502955fe5b141034a566980c2883c0aee3bfd
kiruba14112000 <kirubalovely2@gmail.com>
2022-04-27 12:07:49 +0530

    kiruba

---
+++ src/org/ki/git.java
@@ -0,0 +1,12 @@
+package org.ki;
+
+public class git {
+	public void class1() {
+		System.out.println("kiruba123");
+	}
+	public static void main(String[] args) {
+		git g=new git();
+		g.class1();
+	}
+
+}
